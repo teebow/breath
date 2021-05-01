f0d1290ac95ebde080091210a2c65759d9f8009a
GitHub Action <action@github.com>
2021-05-01 17:56:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/teebow/breath/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/teebow/breath/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.teebow.breath.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.teebow.breath.MyBundle
 
 class MyApplicationService {
 
